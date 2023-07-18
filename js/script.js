a84f6abe95d9f362ab4938ba24f372377bceb1b0
raihanprojecthub <raihanramadhanp09@gmail.com>
2023-07-18 09:23:40 +0700

    twilight coffee shop

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// Click outside the element
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
